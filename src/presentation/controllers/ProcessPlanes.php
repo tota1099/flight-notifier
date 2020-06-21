9d75017d5109292f14a454213ffb1e98334c4e58
Renan Porto <renan.porto1099@gmail.com>
2020-06-21 19:20:42 -0300

    feat: ensure ProcessPlanes data param is a JSON

--- src/presentation/controllers/ProcessPlanes.php
+++ src/presentation/controllers/ProcessPlanes.php
@@ -3,12 +3,16 @@
 namespace App\presentation\controllers;
 
 use App\presentation\exceptions\ProcessPlanesException;
+use App\presentation\helpers\Helper;
 
 class ProcessPlanes {
 
   public static function handle(String $data) {
     if(empty($data)) {
       throw new ProcessPlanesException('Data param is required!');
+    }
+    if(!Helper::isValidJson($data)){
+      throw new ProcessPlanesException('The param data needs to be a json!');
     }
   }
 }
---
+++ src/presentation/helpers/Helper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\presentation\helpers;
+
+class Helper {
+
+  public static function isValidJson($string) {
+    json_decode($string);
+    return (json_last_error() == JSON_ERROR_NONE);
+  }
+}
