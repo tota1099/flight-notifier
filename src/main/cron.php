22f02958666348b36830fed9a3063aa1bde99f53
Renan Porto <renan.porto1099@gmail.com>
2020-06-22 15:09:51 -0300

    refactor: remove not used classes

--- src/main/cron.php
+++ src/main/cron.php
@@ -7,10 +7,7 @@ require_once(__DIR__ . '/../../vendor/autoload.php');
 use App\utils\Requests\CurlRequest;
 use App\utils\Notifiers\Telegram\TelegramNotifier;
 use App\presentation\controllers\NotifyPlanes;
-use App\domain\usecases\PlaneModel;
 use App\domain\usecases\Planes;
-use App\domain\usecases\Airport;
-use App\domain\usecases\PlaneTypes;
 use App\presentation\controllers\ProcessPlanes;
 
 $apiPlanes = file_get_contents("json_example.txt");
