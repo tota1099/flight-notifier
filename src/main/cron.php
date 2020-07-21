00828a977c83e5f56672f66aa4d7bcd4eb9dbc72
Renan Porto <renan.porto1099@gmail.com>
2020-07-21 20:13:14 -0300

    refactor: ensure Cron pass PlaneMessage as dependency

--- src/main/cron.php
+++ src/main/cron.php
@@ -8,14 +8,16 @@ use App\config\Config;
 use App\presentation\controllers\NotifyPlanes;
 use App\presentation\services\ProcessPlanes;
 use App\main\factories\NotifierFactory;
+use App\presentation\services\PlaneMessage;
 
 Config::init();
 
-$apiPlanes = file_get_contents("data/planes.json");
+$apiPlanes = file_get_contents("data/planes.txt");
 $planesProcessed = ProcessPlanes::handle($apiPlanes);
+$planeMessage = new PlaneMessage();
 
 if(!empty($planesProcessed)) {
   $notifier = NotifierFactory::createNotifier();
-  $notifyPlanes = new NotifyPlanes($notifier);
+  $notifyPlanes = new NotifyPlanes($notifier, $planeMessage);
   $notifyPlanes->handle($planesProcessed);
 }
