1998dbefb4677dd06604db24aef4775088ea6e54
Renan Porto <renan.porto1099@gmail.com>
2022-11-08 22:42:53 -0300

    feat: set default timezone

--- src/config/Config.php
+++ src/config/Config.php
@@ -6,6 +6,7 @@ use Dotenv\Dotenv;
 
 class Config {
   static function init() {
+    date_default_timezone_set('America/Sao_Paulo');
     $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
     $dotenv->load();
   }
